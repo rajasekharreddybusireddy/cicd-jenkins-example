8a5a53bb1d22aaca1b620303d83a12df3d4a12e5
rajasekhar reddy <rajasekharreddy.busi@gmail.com>
2019-10-06 15:12:34 +0530

    updated

--- src/main/java/com/blogspot/javabyrajasekhar/jenkins/cicdjenkinsexample/CicdJenkinsExampleApplication.java
+++ src/main/java/com/blogspot/javabyrajasekhar/jenkins/cicdjenkinsexample/CicdJenkinsExampleApplication.java
@@ -1,4 +1,4 @@
-package com.techprimers.jenkins.cicdjenkinsexample;
+package com.blogspot.javabyrajasekhar.jenkins.cicdjenkinsexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/blogspot/javabyrajasekhar/jenkins/cicdjenkinsexample/HelloController.java
+++ src/main/java/com/blogspot/javabyrajasekhar/jenkins/cicdjenkinsexample/HelloController.java
@@ -1,4 +1,4 @@
-package com.techprimers.jenkins.cicdjenkinsexample;
+package com.blogspot.javabyrajasekhar.jenkins.cicdjenkinsexample;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello(){
-        return "Hello busireddy";
+        return "Hello welcome to jenkins pivotal example";
     }
 }
